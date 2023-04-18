aa37f2e0cf47c6a1f9a2ccf25050921c23153a17
Pamelaanang <pamela.anang@mail.mcgill.ca>
2023-04-17 23:09:08 -0400

    done

--- footer.php
+++ footer.php
@@ -16,26 +16,26 @@
           <div class="row">
             <!-- Footer About Text-->
             <div class="col">
-              <h6 class="">Copyright</h6>
+              <h6 class=""><a href='tos.php' style='text-decoration: none; color:white'>Copyright</a></h6>
             </div>
             <div class="col">
-              <h6 class="">About Us</h6>
+              <h6 class=""><a href='about.php' style='text-decoration: none; color:white'>About Us</a></h6>
             </div>
             <div class="col">
-              <h6 class="">Facebook</h6>
+              <h6 class=""><a href='https://www.facebook.com' target="_blank" style='text-decoration: none; color:white'>Facebook</a></h6>
             </div>
           </div>
           <br>
           <div class="row">
             <!-- Footer About Text-->
             <div class="col">
-              <h6 class="">Email</h6>
+              <h6 class=""><a href='mailto:support@inkhack.com' style='text-decoration: none; color:white'>Email</a></h6>
             </div>
             <div class="col">
-              <h6 class="">Privacy Policy</h6>
+              <h6 class=""><a href='tos.php' style='text-decoration: none; color:white'>Privacy Policy</a></h6>
             </div>
             <div class="col">
-              <h6 class="">Twitter</h6>
+              <h6 class=""><a href='https://twitter.com' target="_blank" style='text-decoration: none; color:white'>Twitter</a></h6>
             </div>
           </div>
 
@@ -44,26 +44,26 @@
           <div class="row">
             <!-- Footer About Text-->
             <div class="col">
-              <h6 class="">Phone</h6>
+              <h6 class=""><a href='tel:+15459098876' style='text-decoration: none; color:white'>Phone</a></h6>
             </div>
             <div class="col">
-              <h6 class="">Terms of Service</h6>
+              <h6 class=""><a href='tos.php' style='text-decoration: none; color:white'>Terms of Service</a></h6>
             </div>
             <div class="col">
-              <h6 class="">Instagram</h6>
+              <h6 class=""><a href='https://www.instagram.com' target="_blank" style='text-decoration: none; color:white'>Instagram</a></h6>
             </div>
           </div>
           <br>
           <div class="row">
             <!-- Footer About Text-->
             <div class="col">
-              <h6 class="">Address</h6>
+              <h6 class=""><a href='https://goo.gl/maps/G7EdWB1i7PyaWNh26' target="_blank" style='text-decoration: none; color:white'>Address</a></h6>
             </div>
             <div class="col">
-              <h6 class="">Feedback</h6>
+              <h6 class=""><a href='' style='text-decoration: none; color:white'>Feedback</a></h6>
             </div>
             <div class="col">
-              <h6 class="">Discord</h6>
+              <h6 class=""><a href='https://discord.com'target="_blank" style='text-decoration: none; color:white'>Discord</a></h6>
             </div>
           </div>
 
--- header.php
+++ header.php
@@ -20,17 +20,12 @@ $l_ip = get_lec_IP();
           style='padding-left: 60px;' width="180" height="40" class="logo" alt="">
       </a>
     </div>
-    <!-- <a class="navbar-brand" href="#"><img class="logo" align="left" alt="InkHacks" height="38" width="100"
-          src="Assets/img/Logo_tp.png"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon mx-auto"></span>
-      </button> -->
+   
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" style='color:white;font-family:Source Serif Pro; padding-right: 100px'
-            aria-current="page" href="#">About</a>
+            aria-current="page" href="about.php">About</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" style='color:white;font-family:Source Serif Pro; padding-right: 100px'
