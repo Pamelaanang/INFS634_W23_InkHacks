30ba6414d8918d8f3e737506b7b61cd10cd6e4ba
Pamelaanang <pamela.anang@mail.mcgill.ca>
2023-04-21 21:30:59 -0400

    Final commit

---
+++ tos.php
@@ -0,0 +1,126 @@
+<!Doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ink Hacks</title>
+    <link href='https://fonts.googleapis.com/css?family=Source Serif Pro' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="Assets/css/Mystyle.css" />
+</head>
+
+<body>
+    <div id="main">
+        <div class='container-fluid' id='cfluid' style='border:1px solid black;'>
+            <div class='row'>
+                <div class="col-10" style='padding-left:50px;height:20%;padding-top:20px'>
+                    <br>
+                    <h2><b>Terms of Service</b></h2>
+                    <small>Thank you for considering our services. Below you will find the sample terms of service (TOS)
+                        content:</small>
+                    <div class='row'>
+                        <div style='padding-top:25px;padding-bottom:20px'>
+
+                            <a href="signup.php" style='text-decoration:none; color:black'> Sign Up Here!</a>
+
+                            <i class="bi bi-arrow-right"></i>
+
+                            <button onclick="window.location.href = 'index.php';" class='btn btn-warning'
+                                style='padding-top:2px; height:30px;color:black; background-color:#FEBA33; border-radius:10px'>
+                                Home</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div style='text-align:left;color:black; padding-left:50px;padding-right:50px'>
+            <center>
+                <h3>(TOS)</h3>
+            </center>
+
+            Introduction:<br>
+            Welcome to our services. These terms of service (“TOS”) govern your access to and use of our services,
+            including any updates, new features, or enhancements to our services. By using our services, you agree to be
+            bound by these TOS. If you do not agree to these TOS, please do not use our services.
+            <br><br>
+
+            User Conduct:<br><br>
+            You agree not to use our services to:
+            violate any applicable laws, regulations, or third-party rights;
+            upload or transmit any content that is unlawful, harmful, threatening, abusive, harassing, defamatory,
+            vulgar, obscene, libelous, invasive of another’s privacy, or otherwise objectionable;
+            transmit any viruses, worms, defects, Trojan horses, or other items of a destructive nature;
+            interfere with the operation of our services or the servers or networks used to make our services available;
+            attempt to gain unauthorized access to our services, user accounts, or computer systems;
+            harass or interfere with any other user’s use and enjoyment of our services;
+            use our services for any fraudulent or inappropriate purpose;
+            engage in any activity that could damage or impair our services, including but not limited to the use of
+            automated systems or software to access, copy or monitor any part of our services.
+            <br><br>
+            User Content:<br>
+            You are solely responsible for any content that you upload, transmit or otherwise make available through our
+            services. You agree not to upload or transmit any content that infringes any third-party intellectual
+            property rights, privacy rights or publicity rights. We reserve the right to remove any content that we
+            believe, in our sole discretion, violates these TOS.<br><br>
+
+            Intellectual Property:<br>
+            All content included on our services, such as text, graphics, logos, images, audio clips, video clips, and
+            software, is our property or the property of our content suppliers and protected by applicable intellectual
+            property laws.<br><br>
+
+            Disclaimer of Warranties:<br>
+            Our services are provided “as is” and “as available.” We make no representations or warranties of any kind,
+            express or implied, as to the operation of our services or the information, content, materials, or products
+            included on our services.<br><br>
+
+            Limitation of Liability:<br>
+            In no event shall we be liable for any direct, indirect, incidental, special, or consequential damages
+            arising out of or in any way connected with the use of our services or with the delay or inability to use
+            our services, even if we have been advised of the possibility of such damages.<br><br>
+
+            Termination:<br>
+            We may terminate or suspend your access to our services at any time, without notice and without liability,
+            for any reason, including but not limited to a breach of these TOS.<br><br>
+
+            Governing Law and Dispute Resolution:<br>
+            These TOS shall be governed by and construed in accordance with the laws of the state or country where we
+            are located. Any dispute arising out of or in connection with these TOS shall be resolved through binding
+            arbitration in accordance with the rules of the American Arbitration Association.<br><br>
+
+            Modifications:<br>
+            We reserve the right to modify these TOS at any time without notice. Your continued use of our services
+            following any modification shall be deemed to constitute acceptance of such modification.<br><br>
+
+            Entire Agreement:<br>
+            These TOS constitute the entire agreement between you and us with respect to the use of our services and
+            supersede all prior or contemporaneous communications and proposals, whether oral or written, between you
+            and us with respect to the use of our services.<br>
+        </div>
+
+
+    </div>
+
+
+    </div>
+    </div>
+    </div>
+    <br>
+
+
+
+    <?php include('footer.php'); ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+
+</body>
+
+
+</html>
